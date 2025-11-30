244b9d538fc3d56707992854342d7d358f7cedd4
soman96 <ahmad@soman.ca>
2025-11-30 16:57:42 -0500

    Updated user controller for JWT  tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,32 @@
 const UserModel = require("../models/userModel");
 
+// JWT tokens
+const jwt = require("jsonwebtoken");
+
+// Function for creating JWT tokens
+const generateToken = (user) => {
+  return jwt.sign(
+    { userId: user._id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+};
+
 // Signup Controller
 exports.signup = async (req, res) => {
   const newUser = new UserModel(req.body);
 
   try {
     const savedUser = await newUser.save();
-    res.status(201).json({ message: "User created successfully", user_id: savedUser._id });
+
+    // Generate the token upon successful signup
+    const token = generateToken(savedUser);
+
+    res.status(201).json({
+      message: "User created successfully",
+      user_id: savedUser._id,
+      token
+    });
   } catch (error) {
     res.status(500).json({
       status: "false",
@@ -20,7 +40,12 @@ exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await UserModel.findOne({ $or: [{ email: email.toLowerCase() }, { username: email.toLowerCase() }] }).select("+password");
+    const user = await UserModel.findOne({
+      $or: [
+        { email: email.toLowerCase() },
+        { username: email.toLowerCase() }
+      ],
+    }).select("+password");
 
     if (!user) {
       return res.status(401).json({
@@ -29,6 +54,7 @@ exports.login = async (req, res) => {
       });
     }
 
+    // Verify password
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
       return res.status(401).json({
@@ -37,7 +63,15 @@ exports.login = async (req, res) => {
       });
     }
 
-    res.status(200).json({ message: "Login successful", user_id: user._id });
+    // Generate JWT token upon successful login
+    const token = generateToken(user);
+
+    res.status(200).json({
+      message: "Login successful",
+      user_id: user._id,
+      token
+    });
+
   } catch (error) {
     res.status(500).json({
       status: "false",
